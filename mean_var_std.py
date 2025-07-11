1c910ea0cee05593717c8e4c36893c84bfc584db
Andy-Raposo <andyraposo24@gmail.com>
2025-07-11 19:27:30 +0200

    Update mean_var_std.py
    Fixed several test-crashing issues, but results are equally valid (rounded values cannot be tested valid)

--- mean_var_std.py
+++ mean_var_std.py
@@ -8,23 +8,24 @@ def calculate(list):
     # To make sure we get a 3x3 array
     if len(list) != 9:
         raise ValueError("List must contain nine numbers.")
+    
     matrix = np.array(list, dtype=float).reshape(3, 3)
     
     # The following functions calculate different relevant statistical tools in columns, rows and the whole matrix
     # Finds the mean
-    calculations['mean'] = [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), float(matrix.mean())]
+    calculations['mean'] = [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean()]
     # Finds the variance
-    calculations['variance'] = [np.round(matrix.var(axis=0), 2).tolist(), np.round(matrix.var(axis=1), 2).tolist(), float(np.round(matrix.var(), 2))]
+    calculations['variance'] = [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var()]
     # Finds the std
-    calculations['standard deviation'] = [np.round(matrix.std(axis=0), 2).tolist(), np.round(matrix.std(axis=1), 2).tolist(), float(np.round(matrix.std(), 2))]
+    calculations['standard deviation'] = [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std()]
     # Finds the max value
-    calculations['max'] = [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), float(matrix.max())]
+    calculations['max'] = [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max()]
     # Finds the min value
-    calculations['min'] = [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), float(matrix.min())]
+    calculations['min'] = [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min()]
     # Finds the sum of all values
-    calculations['sum'] = [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), float(matrix.sum())]
+    calculations['sum'] = [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum()]
 
     return calculations
 
-
+# Example usage
 pprint.pprint(calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
